6e20c332124567636dcad9dc6a2ae691e7f7be31
gabrielJohansson <gabrielkri@hotmail.com>
2018-09-13 10:51:10 -0300

    DateTime Picker pronto

--- LoclaPista/Controllers/ComposicaoCorridasController.cs
+++ LoclaPista/Controllers/ComposicaoCorridasController.cs
@@ -49,98 +49,97 @@ namespace LoclaPista.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(int carroid, int carroid2, int carroid3, int carroid4,int pagamentoid,int pistaid,string data,string hora,int id, ComposicaoCorrida composicaoCorrida)
+        public ActionResult Create(int carroid, int carroid2, int carroid3, int carroid4,int pagamentoid,int pistaid,string data,int id, ComposicaoCorrida composicaoCorrida)
         {
             try
             {
-                var dateTime = DateTime.ParseExact(hora, "HH:mm", null, DateTimeStyles.None);
-                var dta = DateTime.Parse(data);
-                if (dta>DateTime.Now && dateTime.Hour<=18 && dateTime.Hour >= 8)
+                DateTime dta = DateTime.ParseExact(data, "dd/MM/yyyy HH:mm",
+                                       CultureInfo.InvariantCulture);
+
+
+                HorarioPista ho = HorarioPistaDAO.ProcurarbyDataHoraPista(dta, pistaid);
+                if (ho == null)
                 {
-                    DateTime combined = dta.Date.Add(dateTime.TimeOfDay);
-                    HorarioPista ho = HorarioPistaDAO.ProcurarbyDataHoraPista(combined, pistaid);
-                    if (ho==null)
+                    string guid = System.Guid.NewGuid().ToString();
+                    TabelaCarroPessoa teste1 = CarroPessoaDao.ProcurarbyId(carroid);
+                    TabelaCarroPessoa teste2 = CarroPessoaDao.ProcurarbyId(carroid2);
+                    TabelaCarroPessoa teste3 = CarroPessoaDao.ProcurarbyId(carroid3);
+                    TabelaCarroPessoa teste4 = CarroPessoaDao.ProcurarbyId(carroid4);
+                    //verificar se nao repete ngm
+                    //ver se os carros sao diferentes
+                    //ver se no dia no horario e a pista estao vagaas
+                    if (ModelState.IsValid)
                     {
-                        string guid = System.Guid.NewGuid().ToString();
-                        TabelaCarroPessoa teste1 = CarroPessoaDao.ProcurarbyId(carroid);
-                        TabelaCarroPessoa teste2 = CarroPessoaDao.ProcurarbyId(carroid2);
-                        TabelaCarroPessoa teste3 = CarroPessoaDao.ProcurarbyId(carroid3);
-                        TabelaCarroPessoa teste4 = CarroPessoaDao.ProcurarbyId(carroid4);
-                        //verificar se nao repete ngm
-                        //ver se os carros sao diferentes
-                        //ver se no dia no horario e a pista estao vagaas
-                        if (ModelState.IsValid)
+                        if (teste1.c.placa != teste2.c.placa | teste1.c.placa != teste3.c.placa | teste1.c.placa != teste4.c.placa | teste2.c.placa != teste3.c.placa | teste2.c.placa != teste4.c.placa | teste3.c.placa != teste4.c.placa | teste1.p.Cpf != teste2.p.Cpf | teste1.p.Cpf != teste3.p.Cpf | teste1.p.Cpf != teste4.p.Cpf | teste2.p.Cpf != teste3.p.Cpf | teste2.p.Cpf != teste4.p.Cpf | teste3.p.Cpf != teste4.p.Cpf)
                         {
-                            if (teste1.c.placa != teste2.c.placa | teste1.c.placa != teste3.c.placa | teste1.c.placa != teste4.c.placa | teste2.c.placa != teste3.c.placa | teste2.c.placa != teste4.c.placa | teste3.c.placa != teste4.c.placa | teste1.p.Cpf != teste2.p.Cpf | teste1.p.Cpf != teste3.p.Cpf | teste1.p.Cpf != teste4.p.Cpf | teste2.p.Cpf != teste3.p.Cpf | teste2.p.Cpf != teste4.p.Cpf | teste3.p.Cpf != teste4.p.Cpf)
-                            {
-                                ComposicaoCorrida t1 = new ComposicaoCorrida();
-                                ComposicaoCorrida t2 = new ComposicaoCorrida();
-                                ComposicaoCorrida t3 = new ComposicaoCorrida();
-                                ComposicaoCorrida t4 = new ComposicaoCorrida();
+                            ComposicaoCorrida t1 = new ComposicaoCorrida();
+                            ComposicaoCorrida t2 = new ComposicaoCorrida();
+                            ComposicaoCorrida t3 = new ComposicaoCorrida();
+                            ComposicaoCorrida t4 = new ComposicaoCorrida();
 
-                                t1.p = teste1.p;
-                                t2.p = teste2.p;
-                                t3.p = teste3.p;
-                                t4.p = teste4.p;
+                            t1.p = teste1.p;
+                            t2.p = teste2.p;
+                            t3.p = teste3.p;
+                            t4.p = teste4.p;
 
 
-                                t1.c = teste1.c;
-                                t2.c = teste2.c;
-                                t3.c = teste3.c;
-                                t4.c = teste4.c;
+                            t1.c = teste1.c;
+                            t2.c = teste2.c;
+                            t3.c = teste3.c;
+                            t4.c = teste4.c;
 
-                                t1.ComposicaoGuid = guid.ToString();
-                                t2.ComposicaoGuid = guid.ToString();
-                                t3.ComposicaoGuid = guid.ToString();
-                                t4.ComposicaoGuid = guid.ToString();
+                            t1.ComposicaoGuid = guid.ToString();
+                            t2.ComposicaoGuid = guid.ToString();
+                            t3.ComposicaoGuid = guid.ToString();
+                            t4.ComposicaoGuid = guid.ToString();
 
 
 
-                                ComposicaoDAO.AdicionarNovo(t1);
-                                ComposicaoDAO.AdicionarNovo(t2);
-                                ComposicaoDAO.AdicionarNovo(t3);
-                                ComposicaoDAO.AdicionarNovo(t4);
+                            ComposicaoDAO.AdicionarNovo(t1);
+                            ComposicaoDAO.AdicionarNovo(t2);
+                            ComposicaoDAO.AdicionarNovo(t3);
+                            ComposicaoDAO.AdicionarNovo(t4);
 
-                                Corrida corrida = new Corrida();
-                                corrida.ComposicaoGuid = guid.ToString();
-                                corrida.Pista = PistasDAO.ProcurarbyId(pistaid);
-                                corrida.Preco =20.00;
-                                corrida.DtaCadastro = DateTime.Now;
-                                corrida.DtaCorrida = combined;
-                                corrida.DtaCancelamento = corrida.DtaCadastro;
-                                corrida.Responsavel = PessoasDAO.ProcurarbyId(id);
-                                corrida.pagamento = PagamentoDAO.ProcurarbyId(pagamentoid);
+                            Corrida corrida = new Corrida();
+                            corrida.ComposicaoGuid = guid.ToString();
+                            corrida.Pista = PistasDAO.ProcurarbyId(pistaid);
+                            corrida.Preco = 20.00;
+                            corrida.DtaCadastro = DateTime.Now;
+                            corrida.DtaCorrida = dta;
+                            corrida.DtaCancelamento = corrida.DtaCadastro;
+                            corrida.Responsavel = PessoasDAO.ProcurarbyId(id);
+                            corrida.pagamento = PagamentoDAO.ProcurarbyId(pagamentoid);
 
-                                CorridaDAO.AdicionarNovo(corrida);
+                            CorridaDAO.AdicionarNovo(corrida);
 
-                                HorarioPista horariopista = new HorarioPista();
-                                horariopista.pista= PistasDAO.ProcurarbyId(pistaid);
-                                horariopista.Hora_inicial = combined;
-                                horariopista.Hora_Final = combined.AddHours(1);
+                            HorarioPista horariopista = new HorarioPista();
+                            horariopista.pista = PistasDAO.ProcurarbyId(pistaid);
+                            horariopista.Hora_inicial = dta;
+                            horariopista.Hora_Final = dta.AddHours(1);
 
-                                HorarioPistaDAO.AdicionarNovo(horariopista);
+                            HorarioPistaDAO.AdicionarNovo(horariopista);
 
 
-                                return RedirectToAction("Index","Pessoas");
-                            }
-                            ModelState.AddModelError("", "Alguma Coisa está repetida(Competidor ou Carro)");
-                            return RedirectToAction("Create");
+                            return RedirectToAction("Index", "Pessoas");
                         }
+                        ModelState.AddModelError("", "Alguma Coisa está repetida(Competidor ou Carro)");
+                        return RedirectToAction("Create");
                     }
-                    ModelState.AddModelError("", "Horário Ocupado");
-                    return RedirectToAction("Create");
-
                 }
+                ModelState.AddModelError("", "Horário Ocupado");
+                return RedirectToAction("Create");
+
+
                 ModelState.AddModelError("", "Data ou Hora Já Passou");
                 return RedirectToAction("Create");
-        }
+            }
             catch
             {
                 ModelState.AddModelError("", "Hora Inválida/Dia Inválido");
 
                 return RedirectToAction("Create");
-    }
-}
+            }
+        }
 
 // GET: ComposicaoCorridas/Edit/5
 public ActionResult Edit(int? id)
